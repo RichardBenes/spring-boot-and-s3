94463d173bc6e0698fdb84cf0499f1e9766453d8
Richard Beneš <richard.benes@monster.com>
2022-06-02 12:40:54 +0200

    Spring Boot is there & working

--- src/main/java/com/richardbenes/awstutorials/App.java
+++ src/main/java/com/richardbenes/awstutorials/App.java
@@ -1,13 +1,16 @@
 package com.richardbenes.awstutorials;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
+import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @EnableAutoConfiguration
+@SpringBootApplication
+public class App {
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
     }
+    
 }
---
+++ src/main/java/com/richardbenes/awstutorials/controllers/MyController.java
@@ -0,0 +1,15 @@
+package com.richardbenes.awstutorials.controllers;
+
+import java.time.Instant;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyController {
+
+    @GetMapping("/home1")
+    ResponseEntity<?> home() {
+        return ResponseEntity.ok("You are home1 at " + Instant.now());
+    }    
+}
